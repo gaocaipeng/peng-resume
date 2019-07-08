d39945529074305800febd6f145f3fc29b31b1ab
gaocaipeng <2540225402@qq.com>
2019-07-08 15:50:15 +0800

    resume

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# peng-resume-
+# peng-resume
 Gao Caipeng's resume website yo~
+
+姓名：高彩鹏
+出生日期：1995.04
+Iphone：18813105216
+邮箱：gao.xiaotian@hotmail.com
+个人网站：www.gaocaipeng.com
+1.	熟练掌握 HTML 及 HTML5 新特性，CSS 及 CSS3 新增属性
+2.	熟悉响应式设计、Sass，Less 预处理器，掌握 JavaScript、ES5、ES6 等技术。
+3.	熟悉 Node.js 基于Koa，Express来实现前后端分离开发，MySQL 数据库，gulp、webpack 等自动化构建工具 ，熟悉前端优化需求。
+4.	熟练运用 jQuery 库，可使用 Better-Scroll，Swiper，ElementUI，Ant design 等实现页面交互。
+5.	熟练使用Vue、React、Dva等主流前端框架及其生态链，熟练使用  Vuex、Redux做状态管理。
+6.	熟悉微信小程序从开发到上线流程，掌握基本的小程序API文档，熟练运用taro框架。
+7.	熟练使用Postman等接口调试工具、Fiddler抓包工具、Canvas画图、Echarts数据可视化。
+8.	对前端技术的发展趋势保持关注，拥有较强的学习能力，热衷前端技术研究，善于解决各类疑难杂症。
+
---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log("%c\n       ", "font-size:41px;background:url('https://wx4.sinaimg.cn/mw690/006gEs1yly1g4cj9giehdj30u10u077k.jpg') no-repeat;padding-left:400px;padding-bottom: 240px;");
+        cons.log('世界这么大我想去浪浪~\n欢迎您观看我的简历，多多指教~\n很期待加入本公司任职~\n共同进步，一起学习！');
+        cons.log("邮件地址：%cgao.xiaotian@hotmail.com", "color:red;font-weight:bold;");
+    }
+}
+// function fuckyou(){
+//     window.close(); //关闭当前窗口(防抽)
+//    window.location="about:blank"; //将当前窗口跳转置空白页
+// }
+// function ck() {
+//   console.profile();
+//   console.profileEnd();
+//   //我们判断一下profiles里面有没有东西，如果有，肯定有人按F12了，没错！！
+//   if(console.clear) { console.clear() };
+//                       if (typeof console.profiles =="object"){
+//   return console.profiles.length > 0;
+//                       }
+// }
+// function hehe(){
+// if( (window.console && (console.firebug || console.table && /firebug/i.test(console.table()) )) || (typeof opera == 'object' && typeof opera.postError == 'function' && console.profile.length > 0)){
+// fuckyou();
+// }
+// if(typeof console.profiles =="object"&&console.profiles.length > 0){
+// fuckyou();
+// }
+// }
+// hehe();
+// window.onresize = function(){
+// if((window.outerHeight-window.innerHeight)>200)
+// //判断当前窗口内页高度和窗口高度，如果差值大于200，那么呵呵
+//  fuckyou();
+// }
